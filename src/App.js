82547cff7cd6675d5bfdd87ca1585cdb8b21a95d
Tornike-Oniani <dragonforwar@gmail.com>
2023-02-09 14:12:18 +0400

    set up project and installed libraries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
-import Navigation from './components/navigation/navigation.component';
-
 import './App.css';
-import HomePage from './pages/home-page/home-page.component';
-import DashboardPage from './pages/dashboard-page/dashboard-page.component';
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
